7fe769de2ce7fe689d84873126b2910fc3b5c391
OneSquareOne <johnls1986@gmail.com>
2022-10-11 22:21:32 -0400

    updates

--- Project 1/Code/BSClientTest.java
+++ Project 1/Code/BSClientTest.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class BSClientTest {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter server name:");
         String serverName = keyboard.next();
         BattleShipClient client = new BattleShipClient(serverName);
         client.runClient();
--- Project 1/Code/BSServerTest.java
+++ Project 1/Code/BSServerTest.java
@@ -3,8 +3,8 @@ public class BSServerTest {
     public static void main(String[] args) {
      
         BattleShipServer server = new BattleShipServer();
+        System.out.println(server.getServerName());
         server.startServer();
-
         System.out.println("Server connected!");
     }
 }
