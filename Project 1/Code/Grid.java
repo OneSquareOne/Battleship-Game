9a5559906d20b6df8ff23bc55ebc1560d496d8c2
RyanCollins2 <103212756+RyanCollins2@users.noreply.github.com>
2022-10-17 17:47:12 -0400

    Update Grid.java

--- Project 1/Code/Grid.java
+++ Project 1/Code/Grid.java
@@ -5,19 +5,25 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;  
 import javax.swing.JFrame;  
-import javax.swing.JLabel;  
-import javax.swing.JPanel;  
-public class JFrameExample {  
-    public static void main(String s[]) {  
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;  
+public class JFrameExample implements ActionListener{  
+    public static void main(String s[]) { 
+    	
+    	
         JFrame frame = new JFrame();
- 
+        frame.setBackground(Color.LIGHT_GRAY);
           
         
         JPanel buttonGrid = new JPanel();
-        buttonGrid.setLayout(new GridLayout(10,10));
+        buttonGrid.setLayout(new GridLayout(11,11));
         
         
         /*-
@@ -26,17 +32,101 @@ public class JFrameExample {
         testpan.setBounds(0, 0, 250, 250);
         frame.add(testpan);
         -*/
-        
-        for(int i = 0; i < 10; i++)
+        String str;
+		JButton temp;
+		
+        for(int i = 0; i < 11; i++)
         {
-        	for(int j = 0; j < 10; j++)
+        	switch (i) {
+            case 0:  
+            	str = "0 0";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 1:  
+            	str = "A";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 2:  
+            	str = "B";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 3:  
+            	str = "C";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 4:  
+            	str = "D";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 5:  
+            	str = "E";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 6:  
+            	str = "F";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 7:  
+            	str = "G";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 8:  
+            	str = "H";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 9:  
+            	str = "I";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+            case 10:  
+            	str = "J";
+	    		temp=new JButton(str);
+	    		temp.setBackground(Color.LIGHT_GRAY);
+	    		buttonGrid.add(temp);
+                break;
+        	}
+        	for(int j = 1; j < 11; j++)
         	{
-        		char c = (char) ('A' + i);
-        		String str = c + " "+(j+1);
-        		buttonGrid.add(new JButton(str));
+        		if(i==0) {
+        			str =""+j;
+            		temp=new JButton(str);
+            		temp.setBackground(Color.LIGHT_GRAY);
+            		buttonGrid.add(temp);
+        		}else {
+	        		char c = (char) ('A' + (i-1));
+	        		String str1 = c + " "+(j);
+	        		temp=new JButton(str1);
+	        		temp.addActionListener(new ActionListener() {
+	                    @Override
+	                    public void actionPerformed(ActionEvent e) {
+	                        JOptionPane.showMessageDialog(frame,str1 );
+	                    }
+	                });
+	        		buttonGrid.add(temp);
+        		}
         	}
         }
-        buttonGrid.setBounds(950, 300, 450, 450);
+        buttonGrid.setBounds(150, 100, 650, 650);
         frame.add(buttonGrid);
         
         
@@ -49,13 +139,12 @@ public class JFrameExample {
         	for(int j = 0; j < 10; j++)
         	{
         		char c = (char) ('A' + i);
-        		String str = c + " "+(j+1);
+        		str = c + " "+(j+1);
         		buttonGrid2.add(new JButton(str));
         	}
         }
-        buttonGrid2.setBounds(150, 100, 650, 650);
+        buttonGrid2.setBounds(950, 300, 450, 450);
         frame.add(buttonGrid2);
-        
         
         
         JPanel boats = new JPanel();
@@ -70,5 +159,11 @@ public class JFrameExample {
         frame.setLayout(null);
         frame.setSize(750, 750);  
         frame.setVisible(true);
-    }  
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}  
 }  
